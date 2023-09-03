5dd328bfe927963b6181f625f666f33df4a61213
diloes <diloes@hotmail.com>
2023-09-03 12:57:54 +0200

    feat: design SearchBox

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,11 +1,13 @@
 import SearchIcon from './SearchIcon'
 
+import './SearchBox.css'
+
 const SearchBox = () => {
   return (
-    <div className=''>
-      <button>Whole, Finland</button>
-      <button>Add guests</button>
-      <button>
+    <div className='font-mulish text-sm flex justify-center'>
+      <button className='buttonLeft p-4'>Whole, Finland</button>
+      <button className='buttonMiddle p-4 text-[#BDBDBD]'>Add guests</button>
+      <button className='buttonRight p-4 '>
         <SearchIcon />
       </button>
     </div>
