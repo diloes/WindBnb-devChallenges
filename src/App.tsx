c01ede64584e2cecd31d64aab0f8b48ff3b5204f
diloes <diloes@hotmail.com>
2023-09-05 18:35:52 +0200

    fix: desing stay preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Logo from './components/Logo'
 import SearchBox from './components/SearchBox'
-import stays from './stays.json'
+import StayPreview from './components/StayPreview'
 
 const App = () => {
   return (
@@ -19,30 +19,7 @@ const App = () => {
           <p className='text-sm'>12+ stays</p>
         </div>
 
-        <div className='grid'>
-          {stays.map((stay) => (
-            <div key={stay.title} className='mb-8'>
-              <img src={stay.photo} alt={stay.title} className='rounded-3xl mb-3' />
-              <div className='flex justify-between mb-2 items-center'>
-                {stay.superHost ? (
-                  <p className='text-[10px] font-extrabold border-2 border-gray-700 rounded-xl p-1'>
-                    SUPERHOST
-                  </p>
-                ) : (
-                  ''
-                )}
-                <p className='text-xs text-[#828282]'>
-                  {stay.type} . {stay.beds} beds
-                </p>
-                <div className='flex gap-1 items-center'>
-                  <img src='/icons8-star-48.png' height={21} width={21} />
-                  <p className='text-[#4F4F4F] text-xs'>{stay.rating}</p>
-                </div>
-              </div>
-              <h2 className='font-semibold text-sm'>{stay.title}</h2>
-            </div>
-          ))}
-        </div>
+        <StayPreview />
       </main>
       <footer></footer>
     </div>
---
+++ src/components/StayPreview.tsx
@@ -0,0 +1,35 @@
+import stays from '../stays.json'
+
+const StayPreview = () => {
+  return (
+    <div className='grid'>
+      {stays.map((stay) => (
+        <div key={stay.title} className='mb-8'>
+          <img src={stay.photo} alt={stay.title} className='rounded-3xl mb-3' />
+          <div className='flex justify-between mb-2 items-center'>
+            <div className='flex items-center gap-2'>
+              {stay.superHost ? (
+                <p className='text-[10px] font-extrabold border-2 border-gray-700 rounded-xl p-1'>
+                  SUPERHOST
+                </p>
+              ) : (
+                ''
+              )}
+              <p className='text-xs text-[#828282]'>
+                {stay.type} . {stay.beds} beds
+              </p>
+            </div>
+
+            <div className='flex gap-1 items-center'>
+              <img src='/icons8-star-48.png' height={21} width={21} />
+              <p className='text-[#4F4F4F] text-xs'>{stay.rating}</p>
+            </div>
+          </div>
+          <h2 className='font-semibold text-sm'>{stay.title}</h2>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default StayPreview
