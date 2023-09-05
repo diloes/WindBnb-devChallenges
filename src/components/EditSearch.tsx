70745a4f20e09ca3190f8672bd33aeec7bd0675d
diloes <diloes@hotmail.com>
2023-09-05 23:39:24 +0200

    feat: add menu and start design

---
+++ src/components/EditSearch.tsx
@@ -0,0 +1,14 @@
+const EditSearch = () => {
+  return (
+    <section className='grid h-[80vh] font-mulish fixed z-50 inset-0 bg-white p-4'>
+      <div className='flex justify-between'>
+        <p className='font-bold text-xs'>Edit your search</p>
+        <p className=''>X</p>
+      </div>
+    </section>
+  )
+}
+
+export default EditSearch
+
+// TODO: buscar la manera para mostrar el componente por encima de todo lo demás
--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,17 +1,23 @@
 import SearchIcon from './SearchIcon'
 
 import './SearchBox.css'
+import EditSearch from './EditSearch'
 
 const SearchBox = () => {
   return (
-    <div className='font-mulish text-sm flex justify-center mb-8'>
-      <button className='buttonLeft p-4'>Whole, Finland</button>
-      <button className='buttonMiddle p-4 text-[#BDBDBD]'>Add guests</button>
-      <button className='buttonRight p-4 '>
-        <SearchIcon />
-      </button>
+    <div>
+      <div className='font-mulish text-sm flex justify-center mb-8'>
+        <button className='buttonLeft p-4'>Whole, Finland</button>
+        <button className='buttonMiddle p-4 text-[#BDBDBD]'>Add guests</button>
+        <button className='buttonRight p-4 '>
+          <SearchIcon />
+        </button>
+      </div>
+
+      <EditSearch />
     </div>
   )
 }
 
 export default SearchBox
+
