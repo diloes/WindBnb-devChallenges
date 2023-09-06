2d97b6e882f632f63820e90713cc83c3615b7d52
diloes <diloes@hotmail.com>
2023-09-06 18:43:01 +0200

    feat: open and close menu

--- src/components/EditSearch.tsx
+++ src/components/EditSearch.tsx
@@ -1,14 +1,12 @@
-const EditSearch = () => {
+const EditSearch = ({ handleMenuClose }: { handleMenuClose: () => void }) => {
   return (
     <section className='grid h-[80vh] font-mulish fixed z-50 inset-0 bg-white p-4'>
-      <div className='flex justify-between'>
+      <div className='flex justify-between items-center h-[5%]'>
         <p className='font-bold text-xs'>Edit your search</p>
-        <p className=''>X</p>
+        <p onClick={handleMenuClose}>X</p>
       </div>
     </section>
   )
 }
 
 export default EditSearch
-
-// TODO: buscar la manera para mostrar el componente por encima de todo lo demás
--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -2,22 +2,38 @@ import SearchIcon from './SearchIcon'
 
 import './SearchBox.css'
 import EditSearch from './EditSearch'
+import { useState } from 'react'
 
 const SearchBox = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleMenuOpen = () => {
+    setMenuOpen(true)
+  }
+
+  const handleMenuClose = () => {
+    setMenuOpen(false)
+  }
+
   return (
     <div>
       <div className='font-mulish text-sm flex justify-center mb-8'>
-        <button className='buttonLeft p-4'>Whole, Finland</button>
-        <button className='buttonMiddle p-4 text-[#BDBDBD]'>Add guests</button>
-        <button className='buttonRight p-4 '>
+        <button onClick={handleMenuOpen} className='buttonLeft p-4'>
+          Whole, Finland
+        </button>
+        <button onClick={handleMenuOpen} className='buttonMiddle p-4 text-[#BDBDBD]'>
+          Add guests
+        </button>
+        <button onClick={handleMenuOpen} className='buttonRight p-4 '>
           <SearchIcon />
         </button>
       </div>
 
-      <EditSearch />
+      {menuOpen && <EditSearch handleMenuClose={handleMenuClose} />}
     </div>
   )
 }
 
 export default SearchBox
 
+//TODO: Poner el estado para abrir y cerrar y la función en los botones, diseñar
