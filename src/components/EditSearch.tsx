7323ad1f7f7eebd6d80c73dd0280e7d34190ae3c
diloes <diloes@hotmail.com>
2023-09-10 14:38:33 +0200

    feat: add location and button search

--- src/components/EditSearch.tsx
+++ src/components/EditSearch.tsx
@@ -1,21 +1,27 @@
-import { Stay } from '../stay.interface'
+import { useState } from 'react'
 import stays from '../stays.json'
 
-// Creamos nuevo array sin ciudades repetidas
+// Crear un nuevo array sin ciudades repetidas
 const uniqueCities = [...new Set(stays.map((stay) => stay.city))]
 
 const EditSearch = ({ handleMenuClose }: { handleMenuClose: () => void }) => {
+  const [location, setLocation] = useState<string>('Helsinki, Finland')
+
+  const handleLocationChange = (city: string) => {
+    setLocation(city)
+  }
+
   return (
-    <section className=' h-[80vh] font-mulish fixed z-50 inset-0 bg-white p-4'>
+    <section className='flex flex-col h-[80vh] font-mulish fixed z-50 inset-0 bg-white p-4'>
       <div className='flex justify-between items-center mb-3'>
         <p className='font-bold text-xs'>Edit your search</p>
         <p onClick={handleMenuClose}>X</p>
       </div>
 
-      <div className='border rounded-lg '>
+      <div className='border rounded-lg'>
         <div className='border-b p-2'>
           <p className='text-[9px] font-extrabold'>LOCATION</p>
-          <p className=' text-sm'>Helsinki, Finland</p>
+          <p className=' text-sm'>{location + ', Finland'}</p>
         </div>
         <div className='p-2'>
           <p className='text-[9px] font-extrabold'>GUESTS</p>
@@ -23,14 +29,25 @@ const EditSearch = ({ handleMenuClose }: { handleMenuClose: () => void }) => {
         </div>
       </div>
 
-      {uniqueCities.map((city: string) => {
-        return (
-          <div className='flex mt-5 px-3'>
-            <img src='/ubicacion.png' />
+      <div className='flex-grow'>
+        {uniqueCities.map((city: string) => (
+          <div
+            className='flex mt-5 px-3'
+            key={city}
+            onClick={() => handleLocationChange(city)}
+          >
+            <img src='/ubicacion.png' alt={`Location icon for ${city}`} />
             <p>{city}, Finland</p>
           </div>
-        )
-      })}
+        ))}
+      </div>
+
+      <button className='m-auto px-6 py-4 rounded-3xl bg-red-500 bg-opacity-90 text-white'>
+        <div className='flex gap-3'>
+          <img src='/searchIcon.png' width={24} />
+          <p>Search</p>
+        </div>
+      </button>
     </section>
   )
 }
